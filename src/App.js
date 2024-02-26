5bf5c19d27f24cc3e5a1c97f5f0d703039924542
Manoj <manojkumar.srirapu@gmail.com>
2024-02-26 14:52:51 +0530

    Add GitHub Pages deployment configuration

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Route, Switch} from 'react-router-dom'
+import {Route, Switch,Redirect} from 'react-router-dom'
 import './App.css';
 import SignUpForm from "./components/SignUpForm"
 import Home  from './components/Home'
@@ -7,6 +7,7 @@ const App = () => (
    <Switch>
     <Route exact path="/signup" component={SignUpForm} />
     <Route exact path = "/" component = {Home} />
+    <Redirect to = "/signup" />
    </Switch>
 )
 
