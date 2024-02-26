68b7b553fe94c546407033101f4a2da111c75c86
Manoj <manojkumar.srirapu@gmail.com>
2024-02-26 23:15:18 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/ui">
+    <BrowserRouter >
       <App />
     </BrowserRouter>
   </React.StrictMode>,
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-
-export default defineConfig({
-  plugins: [reactRefresh()],
-  base: '/ui/', // Change this to your repository name
-});
